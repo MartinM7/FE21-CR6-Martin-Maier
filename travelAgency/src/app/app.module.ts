f59a9878c9c9273ced74fcb56c642be9c58a1f49
Martin Maier <martin.m@gmx.at>
2021-07-16 10:34:28 +0200

    components added

--- travelAgency/src/app/app.module.ts
+++ travelAgency/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { TravelsComponent } from './travels/travels.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    BlogComponent,
+    TravelsComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
