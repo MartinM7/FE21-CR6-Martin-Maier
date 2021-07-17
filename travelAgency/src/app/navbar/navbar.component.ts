be8408fdc8bd4b5c440fa50e5aac620c03a9e7ba
Martin Maier <martin.m@gmx.at>
2021-07-17 12:14:26 +0200

    update

--- travelAgency/src/app/navbar/navbar.component.ts
+++ travelAgency/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  cart: any;
+  constructor(private CS: CartService) { }
 
   ngOnInit(): void {
+    this.cart = this.CS.getItems();
   }
 
 }
